7fb057809279eec6d4a4d62e855d9ff9aa09ad9e
drem <drem234@gmail.com>
2015-04-14 10:04:42 +0800

    task3

---
+++ js/dataAnalysisController.js
@@ -0,0 +1,43 @@
+$(function () {
+    $.getScript("//code.highcharts.com/highcharts.js", function () {
+        $('#chartContainer').highcharts({
+            title: {
+                text: 'Birth in Taiwan',
+                x: -20 //center
+            },
+            subtitle: {
+                text: 'Source: Ministry of the Interiror',
+                x: -20
+            },
+            xAxis: {
+                categories: ['2007', '2008', '2009', '2010', '2011', '2012', '2013']
+            },
+            yAxis: {
+                title: {
+                    text: 'People'
+                },
+                plotLines: [{
+                    value: "70k",
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                //valueSuffix: '¢XC'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'Men',
+                data: [106898, 103937, 99492, 87213, 101943, 118848, 103120]
+            }, {
+                name: 'Female',
+                data: [97516, 94796, 91818, 79673, 94684, 110633, 95993]
+            }]
+        });
+    });
+});
