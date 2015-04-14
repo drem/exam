fda292fe86892b58ab826206d696eb004b1868af
drem <drem234@gmail.com>
2015-04-14 09:05:11 +0800

    task2

--- js/index.js
+++ js/index.js
@@ -11,6 +11,21 @@
 
     /* ui handler */
     $(".menu").on("click", "li", function () {
-        $(this).addClass("clicked").siblings().removeClass("clicked");
+        var $this = $(this),
+            linkId = $this.attr("data-linkId"),
+            preventCache = "?_=" + (+new Date());
+
+        $.get("template/" + linkId + ".html" + preventCache, function (fileContent) {
+            var allContent = externalContent(linkId, preventCache) + fileContent;
+            $("#content").html(allContent);
+        });
+
+        function externalContent(id, preventCache) {
+            var returnValue = [
+                '<link href="css/', id, 'Style.css', preventCache, '" rel="stylesheet" />',
+                '<script type="text/javascript" src="js/', id, 'Controller.js"></script>'// jQuery().html() will handler it.
+            ].join("");
+            return returnValue;
+        };
     });
 });
