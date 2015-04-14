e62b053dfd42980230c6482b8c91b5f3bd73df77
drem <drem234@gmail.com>
2015-04-14 08:05:51 +0800

    task1

---
+++ js/index.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    /* help css select right element */
+    $(".menu li:first-child").addClass("first-child");
+    $(".menu").on("mouseover", "li", function () {
+        $(this).addClass("hover");
+    }).on("mouseout", "li", function () {
+        $(this).removeClass("hover");
+    }).on("click", "li", function () {
+        $(this).addClass("clicked").siblings().removeClass("clicked");
+    });
+
+    /* ui handler */
+    $(".menu").on("click", "li", function () {
+        $(this).addClass("clicked").siblings().removeClass("clicked");
+    });
+});
